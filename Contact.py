9465832f63d69fc2a4e5511b8b9451b8906c3a6a
Minjun Lee <lmjg0407@gmail.com>
2023-07-07 22:05:37 -0500

    Used Dictionary on python for the first time.

---
+++ Contact.py
@@ -0,0 +1,17 @@
+#class Contact:
+#
+#    def __init__(self, FN, LN, PN):
+#        self.FirstName = FN
+#        self.LastName = LN
+#        self.PhoneNum = PN
+#        #possibly a picture next time
+#        #will think of a way to add to emergency contact
+
+contact = {
+    "FirstName": "Minjun",
+    "LastName": "Lee",
+    "PhoneNum": "9206669262"
+}
+
+x = contact["FirstName"]
+print(x)
