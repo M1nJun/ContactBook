2e885527ce1e1528b740206cafa5e3d070297571
Minjun Lee <lmjg0407@gmail.com>
2023-07-11 01:48:31 -0500

    Changed the json file data structure from a nested dictionary with the list as a value, to a depth 3 nested dictionary. And then I adjusted the add_contact, remove_contact codes accordingly. I also created a function named show_contact that shows all of the existing contacts on the json file

--- Contact.py
+++ Contact.py
@@ -4,26 +4,20 @@ def add_contact():
     with open("contact.json") as infile:
         contacts = json.load(infile)
 
+    PN = input("Enter phone number: ")
     FN = input("Enter first name: ")
     LN = input("Enter last name: ")
-    PN = input("Enter phone number: ")
 
-    new_contact = {"FirstName": FN, "LastName": LN, "PhoneNum": PN}
-    contacts["data"].append(new_contact)
+    new_contact = {PN:{"FirstName": FN, "LastName": LN}}
+    contacts["data"].update(new_contact)
 
     #without the indent, it appeared on the json file as just a straight line of data.
     with open("contact.json", "w") as outfile:
         json.dump(contacts, outfile, indent=4)
 
-
 def remove_contact():
     with open("contact.json") as infile:
         contacts = json.load(infile)
-
-    for index, contact in enumerate(contacts["data"]):
-        FN = contact["FirstName"]
-        LN = contact["LastName"]
-        print(f'{index}:{FN}-{LN}')
 
     index_to_delete = input("Enter the index of the person you want to delete: ")
 
@@ -32,12 +26,35 @@ def remove_contact():
     with open("contact.json", "w") as outfile:
         json.dump(contacts, outfile, indent=4)
 
+remove_contact()
 
-def edit_contact():
-    with open("contacts.json", "w") as infile:
-        contacts = json.load(infile)
+# def edit_contact():
+#     with open("contacts.json", "w") as infile:
+#         contacts = json.load(infile)
     
-    
+
+
 
 
 def show_contact():
+    with open("contact.json") as infile:
+        contacts = json.load(infile)
+
+    # for key in contacts["data"]:
+    #     FN = contacts["data"][key]["FirstName"]
+    #     LN = contacts["data"][key]["LastName"]
+    #     print(f'{FN}-{LN}')
+
+    for key,val in contacts["data"].items():
+        PN = key
+        FN = val["FirstName"]
+        LN = val["LastName"]
+        print(f'Phone number is: {PN}\nName is: {FN}-{LN}')
+
+#change the data structure into key:phonenum and value: first, last name
+#make a separate folder for the json files.
+#change the code so that it works with the changed data structure.
+#finish making edit, show contact
+#should make a users.json
+#if the user does not have a username.json file, make one
+#change the function so that it takes in parameters from main.py
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 #just import, I need to do shit like contact.<function name>
 from Contact import *
+import os
 
 if __name__=="main":
+    user_name = input("Login: ")
+
+    try:
+        with open(f'{user_name}.json') as f:
+            contacts = json.load(f)
+    except:
+        os.create(f'(user_name).json')
+
     user_choice = input("Choose operations you want:\n1. Show\n2. Add\n3. Remove\n4. Edit")
-    match user_choice:
-        case 1:
-            show_contact()
-        case 2:
-            add_contact()
-        case 3:
-            remove_contact()
-        case 4:
-            edit_contact()
+    if user_choice == 1:
+        show_contact()
+    if user_choice == 2:
+        add_contact()
+    if user_choice == 3:
+        remove_contact()
+    if user_choice == 4:
+        edit_contact()
 
     
 
